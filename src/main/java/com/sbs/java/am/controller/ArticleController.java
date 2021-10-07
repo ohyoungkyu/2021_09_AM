efbe2bb58e7011e5745c55932158be9d4cbb3090
ohyoungkyu <come8908@gmail.com>
2021-10-07 16:21:16 +0900

    게시물 DAO 도입

--- src/main/java/com/sbs/java/am/controller/ArticleController.java
+++ src/main/java/com/sbs/java/am/controller/ArticleController.java
@@ -8,56 +8,41 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.sbs.java.am.util.DBUtil;
-import com.sbs.java.am.util.SecSql;
+import com.sbs.java.am.service.ArticleService;
 
 public class ArticleController {
 
 	private HttpServletRequest request;
 	private HttpServletResponse response;
 	private Connection con;
-	
+	private ArticleService articleService;
+
 	public ArticleController(HttpServletRequest request, HttpServletResponse response, Connection con) {
 		this.request = request;
 		this.response = response;
-		this.con = con;
+
+		articleService = new ArticleService(con);
 	}
 
 	public void actionList() throws ServletException, IOException {
-
-		
 		int page = 1;
 
 		if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
 			page = Integer.parseInt(request.getParameter("page"));
+			// ?page=일
 		}
 
-		int itemsInAPage = 20;
-
-		int limitFrom = (page - 1) * itemsInAPage;
-
-		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
-		sql.append("FROM article;");
-
-		int totalCount = DBUtil.selectRowIntValue(con, sql);
-
-		int totalpage = (int) Math.ceil((double) totalCount / itemsInAPage);
-
-		sql = SecSql.from("SELECT *");
-		sql.append("FROM article");
-		sql.append("ORDER BY id DESC");
-		sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
-
-		System.out.println(sql);
-
-		List<Map<String, Object>> articleRows = DBUtil.selectRows(con, sql);
-
+		int totalpage = articleService.getForPrintListTotalPage();
+		
+		List<Map<String, Object>> articleRows = articleService.getForPrintArticleRows(page);
+		
 		request.setAttribute("articleRows", articleRows);
 		request.setAttribute("page", page);
 		request.setAttribute("totalpage", totalpage);
 		request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+
+		
 	}
 
 }
---
+++ src/main/java/com/sbs/java/am/service/ArticleService.java
@@ -0,0 +1,43 @@
+package com.sbs.java.am.service;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import com.sbs.java.am.dao.ArticleDao;
+import com.sbs.java.am.util.DBUtil;
+import com.sbs.java.am.util.SecSql;
+
+public class ArticleService {
+	private Connection con;
+	private ArticleDao articleDao;
+
+	public ArticleService(Connection con) {
+		this.con = con;
+		this.articleDao = new ArticleDao(con);
+	}
+	
+	public int getItemsInAPage() {
+		return 3;
+	}
+
+	public int getForPrintListTotalPage() {
+		int itemsInAPage = getItemsInAPage();
+
+		int totalCount = articleDao.getTotalCount();
+			
+		int totalpage = (int) Math.ceil((double) totalCount / itemsInAPage);
+
+		return totalpage;
+	}
+
+	public List<Map<String, Object>> getForPrintArticleRows(int page) {
+		int itemsInAPage = getItemsInAPage();
+		int limitFrom = (page - 1) * itemsInAPage;
+
+		List<Map<String, Object>> articleRows = articleDao.getArticleRows(limitFrom, itemsInAPage);
+
+		return articleRows;
+	}
+
+}
